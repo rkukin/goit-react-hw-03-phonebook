378583a59f1e72b991b5b5555b0e6a85b5c0a442
rkukin <romakukin@gmail.com>
2020-01-29 09:06:36 +0200

    hw2.2 WIP

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import { uuid } from 'uuidv4';
 
-export default class App extends Component{
+export default class App extends Component {
 
   state = {
     contacts: [],
@@ -18,7 +18,6 @@ export default class App extends Component{
   };
 
   handleSubmit = (e) => {
-    console.log(e.target);
     e.preventDefault();
     const { name, number, contacts } = this.state;
 
@@ -34,64 +33,61 @@ export default class App extends Component{
       }));
       this.setState({name: "", number: ""});
     }
-
-
   };
 
-  //TODO
-  handleSearch = (e) => {
-    const {name, value} = e.target;
-    this.setState({ [name]: value });
+  handleDelete = ( contactId) => {
+    const { contacts } = this.state;
 
+    const newContacts = contacts.filter(contact => contact.id !== contactId);
+
+    this.setState({contacts: newContacts});
   };
 
-  handleDelete = (e) => {
+  renderItems = (contacts) => {
+    return contacts.map(contact => (
+        <li key={contact.id}><p>{contact.name}: {contact.number}</p> <button onClick={() => this.handleDelete(contact.id)}>Delete</button></li>
+    ));
   };
 
-  list (){
+  renderContactsList (){
     const { contacts, filter } = this.state;
-    if (filter ==="") {
-      return contacts.map(contact => (
-          <li key={contact.id}>{contact.name}: {contact.number} <button onClick={this.handleDelete}>Delete</button></li>
-        ))
-    } else if (contacts.find(element =>(
-        element.name.toLowerCase().indexOf(filter.toLowerCase())>=0))) {
-      return <p>MATCH!</p>
-    } else {
-      return null
+    const filteredResults = contacts.filter(contact => contact.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1 );
+
+
+    if (filter === "") {
+      return this.renderItems(contacts);
     }
+
+    if (filteredResults.length) {
+      return this.renderItems(filteredResults);
+    }
+
+    return null;
   }
 
 
   render(){
-    const { name, number, filter, contacts } = this.state;
+    const { name, number, filter} = this.state;
 
     return (
       <>
       <h2>PhoneBook</h2>
         <form onSubmit={this.handleSubmit}>
           <h3>Name</h3>
-          <label>Name
             <input type="text" value={name} onChange={this.handleChange} name="name" autoComplete='off'/>
-          </label>
+
           <h3>Number</h3>
-          <label>Number
             <input type="tel" value={number} onChange={this.handleChange} name="number" autoComplete='off'/>
-          </label>
+
           <div>
             <button type="submit">Add contact</button>
-
           </div>
         </form>
           <h3>Contacts</h3>
           <input type="text" value={filter} onChange={this.handleChange} name="filter" autoComplete='off'/>
           <ul>
 
-            {/*{contacts.map(contact => (*/}
-            {/*    <li key={contact.id}>{contact.name}: {contact.number} <button onClick={this.handleDelete}>Delete</button></li>*/}
-            {/*  ))}*/}
-
-            {this.list()}
+            {this.renderContactsList()}
 
           </ul>
 
