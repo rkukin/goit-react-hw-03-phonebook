65103d79e0f4201c1806c1e557f6ba2d8f5d3d03
rkukin <romakukin@gmail.com>
2020-01-30 18:54:52 +0200

    Updates

---
+++ precache-manifest.d14d1c01f3e6dedb24c1ce13b77c1756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692c78f542a31e05cb332ae8c413d413",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "d7f08d3a184b3ead1410",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e04669f3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e04669f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3247dcfe5c62405da87",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.7882bdb6.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
